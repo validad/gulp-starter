1f5766d7efa487c1df7ff7ca836267abd8c0be11
Johannes Troeger <johannes.troeger@gmail.com>
2014-10-27 14:59:39 +0100

    refactor(): adjust to our needs
    indention: adds editorconfig and redindent all files
    scss: renamed folder and removed useless files
    javascript: renamed folder and removed useless files

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,15 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var handleErrors = require('../util/handleErrors');
-var config=require('../config').sass;
+var config = require('../config').sass;
 
 gulp.task('sass', ['images'], function () {
-  return gulp.src(config.src)
+    return gulp.src(config.src)
     .pipe(sass({
-      compass: true,
-      bundleExec: true,
-      sourcemap: true,
-      sourcemapPath: '../sass'
+        cacheLocation: '.sass-cache',
+        loadPath: __dirname
     }))
     .on('error', handleErrors)
     .pipe(gulp.dest(config.dest));
