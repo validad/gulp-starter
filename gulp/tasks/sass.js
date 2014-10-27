1467c4e8a4bcd399a12f4c1f04568f03a6f9ad2c
Johannes Troeger <johannes.troeger@gmail.com>
2014-10-27 15:07:28 +0100

    feat(gulp/sass): use node sass

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,14 +1,11 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 var handleErrors = require('../util/handleErrors');
 var config = require('../config').sass;
 
 gulp.task('sass', ['images'], function () {
     return gulp.src(config.src)
-    .pipe(sass({
-        cacheLocation: '.sass-cache',
-        loadPath: __dirname
-    }))
+    .pipe(sass())
     .on('error', handleErrors)
     .pipe(gulp.dest(config.dest));
 });
